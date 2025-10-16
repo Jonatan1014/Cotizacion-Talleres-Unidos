b82f54c8b6d8f6c615007d1bd922b1691841e0e4
Jonatan Cantillo <lamotomasrrapidadelmundo@gmail.com>
2025-10-15 20:46:48 -0500

    Prueba con el nuevo endpoint v3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,44 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b76bddbcbb8e95dbc73c73d1a584a772a2498136',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b76bddbcbb8e95dbc73c73d1a584a772a2498136',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pclzip/pclzip' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.8.2',
+            ),
+        ),
+        'wapmorgan/cam' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.0.2',
+            ),
+        ),
+        'wapmorgan/unified-archive' => array(
+            'pretty_version' => '1.3.0',
+            'version' => '1.3.0.0',
+            'reference' => '34d342f9735f00e0d6b58f1028965b6e66e018b7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wapmorgan/unified-archive',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
